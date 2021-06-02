bf3c35c65e00e9c93a68df25a58831d7b4541fe1
Sekhar Paladugu <sekharp@gmail.com>
2021-06-02 17:37:23 -0600

    add notes validation to model

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -3,4 +3,5 @@ class Prospect < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :email, format: { with: /^(.+)@(.+)$/, message: "Email invalid" }, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 }
+  validates :notes, length: { maximum: 1000 }
 end
