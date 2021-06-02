ecdf39ea05038a7aa2b97c1dcae582cd548f6314
Sekhar Paladugu <sekharp@gmail.com>
2021-06-02 16:46:32 -0600

    create authentication homepage route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  authenticated :user do
+    root "pages#canvass_records", as: :authenticated_root
+  end
   root 'pages#home'
-  get 'pages/my_canvass_records'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
