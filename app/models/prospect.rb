ed05979f3be68b1836532100fe10e8e0cdc38694
Sekhar Paladugu <sekharp@gmail.com>
2021-06-02 17:36:58 -0600

    add validations to model for email and name

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,3 +1,6 @@
 class Prospect < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :email, format: { with: /^(.+)@(.+)$/, message: "Email invalid" }, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 }
 end
