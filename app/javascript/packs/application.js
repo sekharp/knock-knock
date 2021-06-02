edd2a87494e8062b27bea26b514ed6fd7caa87fa
Sekhar Paladugu <sekharp@gmail.com>
2021-06-02 16:31:13 -0600

    install and add devise for authentication

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require('./hello_react')
 require('bootstrap')
 import 'bootstrap/dist/css/bootstrap'
 
