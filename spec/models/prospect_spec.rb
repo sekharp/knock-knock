10ed402fc6d410bfac0c7fc6bba99533e47c09be
Sekhar Paladugu <sekharp@gmail.com>
2021-06-02 18:58:47 -0600

    add name validation specs

--- spec/models/prospect_spec.rb
+++ spec/models/prospect_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe Prospect, type: :model do
       expect(prospect).to be_valid
     end
   end
+
+  describe "validations" do
+    let(:prospect) { FactoryBot.create(:prospect) }
+
+    it 'should have a name' do
+      prospect.name = nil
+      expect(prospect).to_not be_valid
+    end
+
+    it 'cannot have name longer than 100 characters' do
+      prospect.name = 'A' * 101
+      expect(prospect).to_not be_valid
+    end
+  end
 end
